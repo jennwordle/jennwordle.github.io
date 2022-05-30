490c9ae666c91258da687363bf9d40608ea280d4
zhangtom54321 <zhangtom54321@gmail.com>
2022-05-29 21:58:42 -0700

    Revert "Revert "Changes""
    This reverts commit 285aed1e747254d6e3f0e64c17e1388cdd549a30.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     console.log("Hereddd");
-
+    $.getJSON("resources/five_letter_words.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
 })
