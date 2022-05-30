a788a41fbc5633a52ca81af8500c563822663c10
zhangtom54321 <zhangtom54321@gmail.com>
2022-05-29 22:06:15 -0700

    Changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
     console.log("Hereddd");
-    $.getJSON("resources/five_letter_words.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
-    });
 })
